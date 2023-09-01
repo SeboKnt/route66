743f98772c43594bc61fc77de8fed27cfc5e61e9
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-09-01 14:20:07 +0000

    cahnge dest to src error

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 docker buildx build \
   --platform linux/amd64 \
-  --cache-from type=local,dest=/workspaces/route66/tmp/amd64 \
+  --cache-from type=local,src=/workspaces/route66/tmp/amd64 \
   --cache-to type=local,dest=/workspaces/route66/tmp/amd64 \
   --tag seboknt/66:amd64 \
   --file DOCKERFILE .
@@ -11,7 +11,7 @@ echo "--------------------------------------------------------------------------
 
 docker buildx build \
   --platform linux/arm64 \
-  --cache-from type=local,dest=/workspaces/route66/tmp/arm64 \
+  --cache-from type=local,src=/workspaces/route66/tmp/arm64 \
   --cache-to type=local,dest=/workspaces/route66/tmp/arm64 \
   --tag seboknt/66:arm64 \
   --file DOCKERFILE .
@@ -20,7 +20,7 @@ echo "--------------------------------------------------------------------------
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --cache-from type=local,dest=/workspaces/route66/tmp/amd64 \
-  --cache-from type=local,dest=/workspaces/route66/tmp/arm64 \
+  --cache-from type=local,src=/workspaces/route66/tmp/amd64 \
+  --cache-from type=local,src=/workspaces/route66/tmp/arm64 \
   --tag seboknt/66:dev \
   --file DOCKERFILE .
