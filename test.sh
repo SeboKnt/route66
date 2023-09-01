e6477a84b8fd97fa3c4c1301e3358d6e8124b27b
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-09-01 14:01:29 +0000

    add local file cache

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+docker buildx build \
+  --platform linux/amd64 \
+  --cache-from type=local,dest=/workspaces/route66/tmp/amd64 \
+  --cache-to type=local,dest=/workspaces/route66/tmp/amd64 \
+  --tag seboknt/66:amd64 \
+  --file DOCKERFILE .
+
+echo "-------------------------------------------------------------------------------------------------------"
+
+docker buildx build \
+  --platform linux/arm64 \
+  --cache-from type=local,dest=/workspaces/route66/tmp/arm64 \
+  --cache-to type=local,dest=/workspaces/route66/tmp/arm64 \
+  --tag seboknt/66:arm64 \
+  --file DOCKERFILE .
+
+echo "-------------------------------------------------------------------------------------------------------"
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --cache-from type=local,dest=/workspaces/route66/tmp/amd64 \
+  --cache-from type=local,dest=/workspaces/route66/tmp/arm64 \
+  --tag seboknt/66:dev \
+  --file DOCKERFILE .
